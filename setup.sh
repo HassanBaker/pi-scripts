de13ffd2b85126d515b9dbd3cd05919c847016e6
HassanBaker <hassan.baker@umail.ucc.ie>
2021-02-03 23:29:35 +0000

    forgot og pass

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 ROOT=$1
-PASS=$2
+OGPASS=$2
 USERNAME=$3
+PASS=$4
 
 echo "Welcome to your new install. Upgrading everything to latest..."
 sudo ./upgrade.sh
 echo "Upgrade complete. Installing your programs..."
 sudo ./install-programs.sh
 echo "Your programs are installed. Installing zsh for $ROOT user"
-./install-zsh.sh $ROOT $PASS
+./install-zsh.sh $ROOT $OGPASS
 echo "Your programs are installed. Now creating user $USERNAME"
 ./create-user $USERNAME $PASS
 echo "User hassan is created. Installing zsh for $USERNAME"
